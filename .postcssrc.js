3efdf8c77566bebe9759acaa19a88367852d5b85
suixinzmj <498754144@qq.com>
2019-02-13 20:30:00 +0800

    初始化

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+// postcss工具配置文件 使用js处理样式
+module.exports = {
+  "plugins": {
+    "postcss-import": {},
+    "postcss-url": {},
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
