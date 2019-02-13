02d695acbb02acaaf41c06b252cf500a86cccf84
suixinzmj <498754144@qq.com>
2019-02-13 21:17:01 +0800

    登录部分样式

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/base.css'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
 
 Vue.use(ElementUI)
 
