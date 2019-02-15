30d4d16d28843ce6e2c81938c7d38ff203a76b4c
suixinzmj <498754144@qq.com>
2019-02-15 20:15:58 +0800

    用户列表路由配置

--- src/components/home.vue
+++ src/components/home.vue
@@ -100,7 +100,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
