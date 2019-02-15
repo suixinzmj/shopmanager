432a61137b31b402829e6f83f05c430b0801dda4
suixinzmj <498754144@qq.com>
2019-02-15 09:24:23 +0800

    发送登录请求

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="login-wrap">
-    <el-form class="login-form" label-position="top" label-width="80px" :model="formdate">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
-        <el-input v-model="formdate.username"></el-input>
+        <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input type="password" v-model="formdate.password"></el-input>
+        <el-input type="password" v-model="formdata.password"></el-input>
       </el-form-item>
       <el-button @click.prevent="handleLogin()" class="login-btn" type="success">登录</el-button>
     </el-form>
@@ -17,10 +17,18 @@
 export default {
   data () {
     return {
-      formdate: {
+      formdata: {
         username: '',
         password: ''
       }
+    }
+  },
+  methods: {
+    // 发送登录请求
+    handleLogin () {
+      this.$http.post(`login`, this.formdata).then(res => {
+        console.log(res)
+      })
     }
   }
 }
