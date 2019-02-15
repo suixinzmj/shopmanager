19f7d7e0992e24fef566f81bfd55251b2a8befd0
suixinzmj <498754144@qq.com>
2019-02-15 10:54:18 +0800

    首页头部样式调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -14,13 +26,22 @@ export default {};
 
 <style>
 .container {
-    height: 100%;
-    background-color: red;
+  height: 100%;
+  background-color: #b3c0d1;
 }
 .aside {
-    background-color: greenyellow;
+  background-color: greenyellow;
 }
 .main {
-    background-color:  skyblue;
+  background-color: skyblue;
+}
+.middle {
+    line-height: 60px;
+    text-align: center;
+    color: #fff;
+}
+.logout {
+    line-height: 60px;
+    text-decoration: none;
 }
 </style>
