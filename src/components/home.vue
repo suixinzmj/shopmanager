d1753eea11509193b797cd140dd40a0cb992d79e
suixinzmj <498754144@qq.com>
2019-02-15 20:03:14 +0800

    侧边栏引入导航组件-登录权限验证

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container class="container">
-    <el-header>
+    <el-header class="header">
       <el-row>
         <el-col :span="4">
           <img src="@/assets/logo.png" alt="图片加载失败">
@@ -14,14 +14,108 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <el-menu
+          :router="true"
+          :unique-opened="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="users">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>角色列表</span>
+              </el-menu-item>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>权限管理</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>分类参数</span>
+              </el-menu-item>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>商品分类</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>订单列表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span>数据报表</span>
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  beforeMount () {
+     if (!localStorage.getItem('token')) {
+       this.$router.push({
+         name: 'login'
+       })
+       this.$message.warning('请先登录')
+     }
+  }
+};
 </script>
 
 <style>
@@ -29,19 +123,22 @@ export default {};
   height: 100%;
   background-color: #b3c0d1;
 }
+.header {
+  background-color: #b3c0d1;
+}
 .aside {
-  background-color: greenyellow;
+  background-color: #fff;
 }
 .main {
   background-color: skyblue;
 }
 .middle {
-    line-height: 60px;
-    text-align: center;
-    color: #fff;
+  line-height: 60px;
+  text-align: center;
+  color: #fff;
 }
 .logout {
-    line-height: 60px;
-    text-decoration: none;
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
