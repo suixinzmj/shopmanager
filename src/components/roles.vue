15efc25bdaf07205e776dcdda8672e1c4d02c04b
suixinzmj <498754144@qq.com>
2019-02-18 20:27:41 +0800

    角色列表面包屑+添加角色按钮

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -1,15 +1,21 @@
 <template>
-    <el-card class="box">
-        角色----
-    </el-card>
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
+    <!-- 添加角色按钮 -->
+    <el-button type="success" @click="showDiaAddUser()" class="addRoles">添加角色</el-button>
+  </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.box {
+  height: 100%;
+}
+.addRoles {
+  margin-top: 20px;
+}
 </style>
