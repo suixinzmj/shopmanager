30237aa1866b62061c14426ee0e55950d9aacd2a
suixinzmj <498754144@qq.com>
2019-02-18 20:50:39 +0800

    角色列表表格展示

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -4,11 +4,56 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <!-- 添加角色按钮 -->
     <el-button type="success" @click="showDiaAddUser()" class="addRoles">添加角色</el-button>
+    <!-- 表格 -->
+    <el-table :data="roles" style="width: 100%">
+      <el-table-column type="expand" width="80">
+          <template slot-scope="props"></template>
+      </el-table-column>
+      <el-table-column type="index" label="#" width="180"></el-table-column>
+      <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
+      <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
+      <el-table-column label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+          <el-button
+            @click="showDiaSetRights(scope.row)"
+            type="success"
+            icon="el-icon-check"
+            circle
+            size="mini"
+            plain
+          ></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      roles: []
+    };
+  },
+  created() {
+    this.getRoles();
+  },
+  methods: {
+    showDiaSetRights() {},
+    async getRoles() {
+      const res = await this.$http.get(`roles`);
+      const {
+        meta: { msg, status },
+        data
+      } = res.data;
+      if (status === 200) {
+        this.roles = data;
+      }
+    }
+  }
+};
 </script>
 
 <style>
