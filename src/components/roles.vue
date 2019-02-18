14b63f0c67a922707716ea345f414ff20f0ac4e6
suixinzmj <498754144@qq.com>
2019-02-18 21:15:53 +0800

    角色列表展开行

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -7,7 +7,30 @@
     <!-- 表格 -->
     <el-table :data="roles" style="width: 100%">
       <el-table-column type="expand" width="80">
-          <template slot-scope="props"></template>
+        <template slot-scope="scope">
+          <el-row v-for="(item1, i) in scope.row.children" :key="item1.id" class="level1">
+            <el-col :span="4">
+              <el-tag closable>{{ item1.authName }}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row v-for="(item2, i) in item1.children" :key="item2.id" class="level2">
+                <el-col :span="4">
+                  <el-tag closable type="warning">{{ item2.authName }}</el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="info"
+                    v-for="(item3, i) in item2.children"
+                    :key="item3.id"
+                  >{{ item3.authName }}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
       </el-table-column>
       <el-table-column type="index" label="#" width="180"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
@@ -62,5 +85,9 @@ export default {
 }
 .addRoles {
   margin-top: 20px;
+}
+.level1,
+.level2 {
+  margin-top: 10px;
 }
 </style>
