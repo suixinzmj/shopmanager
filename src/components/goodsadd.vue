a5556397366304ec15338e1009edb1822af59706
suixinzmj <498754144@qq.com>
2019-02-19 20:27:24 +0800

    步骤条完成

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -4,11 +4,25 @@
     <cus-bread level1="商品管理" level2="商品列表"></cus-bread>
     <!-- 添加商品信息提示信息 -->
     <el-alert title="添加商品信息" type="info" center show-icon class="aleart"></el-alert>
+    <!-- 步骤条 -->
+    <el-steps :active="active * 1" align-center>
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+    </el-steps>
   </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            active: "1"
+        }
+    }
+};
 </script>
 
 <style>
@@ -16,6 +30,6 @@ export default {};
   height: 100%;
 }
 .aleart {
-    margin-top: 20px;
+  margin: 20px 0;
 }
 </style>
